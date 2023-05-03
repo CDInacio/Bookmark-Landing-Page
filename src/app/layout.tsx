b10a88253a535bc37258d21fe3f640a3a961a653
claudiodantas <claudiodantas1996@gmail.com>
2023-05-02 21:20:36 -0300

    fix: fix the components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Navbar />
-      <body>{children}</body>
-      <Footer />
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
